addb2d2048d363bdc0cc175b8f4570c72ee273e1
Santosh Rai <santoshrai@Santoshs-MacBook-Pro.local>
2020-08-20 22:33:58 +0900

    Updates

---
+++ precache-manifest.e2a71932d4f8d2fff8bf77b21810da55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55b79beae2092fbc1e21b719f12ae112",
+    "url": "/nevolutiontech/one-sns/index.html"
+  },
+  {
+    "revision": "a781aaf4e79b7d617e14",
+    "url": "/nevolutiontech/one-sns/static/css/main.3e6c01ac.chunk.css"
+  },
+  {
+    "revision": "3a183164528910e2a105",
+    "url": "/nevolutiontech/one-sns/static/js/2.c04c11d9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/nevolutiontech/one-sns/static/js/2.c04c11d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a781aaf4e79b7d617e14",
+    "url": "/nevolutiontech/one-sns/static/js/main.0d20bf2e.chunk.js"
+  },
+  {
+    "revision": "d5a885e88ee3a86729d3",
+    "url": "/nevolutiontech/one-sns/static/js/runtime-main.51b58a34.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/nevolutiontech/one-sns/precache-manifest.e2a71932d4f8d2fff8bf77b21810da55.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nevolutiontech/one-sns/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
